d43e9b6fce2e38d513e3cdffc99043edb9cac494
chinthalliprisci1246-tech <chinthalliprsci1246@gmail.com>
2026-05-23 22:27:10 +0530

    updated cart icon

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -8,7 +8,7 @@ const CartIcon = () => {
   const totalQty = cartItems.reduce((sum, item) => sum + item.qty, 0);
   return (
     <Link href="/cart" className="group relative">
-      <ShoppingCart className="w-5 h-5 hover:text-shop-dark-green hoverEffect" />
+      <ShoppingCart className="w-6 h-6 hover:text-shop-dark-green hoverEffect" />
       <span className="absolute -top-1 -right-1 bg-shop-dark-green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
         {totalQty}
       </span>
